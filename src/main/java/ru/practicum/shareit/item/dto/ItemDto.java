8297c3795db2cb82da4878e45deb3df4a41e1499
Denis <DenisMix1@yandex.ru>
2025-08-17 22:44:15 +0300

    reset branch

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,25 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.*;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+/**
+ * TODO Sprint add-controllers.
+ */
 public class ItemDto {
-    private Long id;
-
-    @NotBlank(message = "Название не может быть пустым")
-    private String name;
-
-    @NotBlank(message = "Описание не может быть пустым")
-    private String description;
-
-    @NotNull(message = "Статус доступности не может быть null")
-    private Boolean available;
-
-    private Long requestId;
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,35 +1,7 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.*;
-
-
-import jakarta.persistence.*;
-import ru.practicum.shareit.user.model.User;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "items")
+/**
+ * TODO Sprint add-controllers.
+ */
 public class Item {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false)
-    private String description;
-
-    @Column(name = "is_available", nullable = false)
-    private Boolean available;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "owner_id", nullable = false)
-    private User owner;
-
-    @Column(name = "request_id")
-    private Long requestId;
 }
